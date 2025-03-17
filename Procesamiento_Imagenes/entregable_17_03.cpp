f0df2f0f5355b438c9fe1d3ab950deb552402464
Daniel Fonseca <fonsecafelipedaniel@gmail.com>
2025-03-16 20:53:46 -0500

    inicio entregable martes

---
+++ Procesamiento_Imagenes/entregable_17_03.cpp
@@ -0,0 +1,26 @@
+#include "Image.h"
+#include <iostream>
+int main(){
+    Image planta("plant2.jpg");
+    Image planta_usable = planta;
+    printf("tamaño del vector para la imagen nueva: %d", planta_usable.w*planta_usable.h);
+    Image planta_vectorizada(planta_usable.w,planta_usable.h,1); //creamos imagen donde se creará la nueva imagen binarizada solo detectando las hojas 
+
+    for (int i = 0, j = 0; i < planta_usable.size; i += planta_usable.channels, j++){
+        int  R = planta_usable.data[i];
+        int  G = planta_usable.data[i+1];
+        int  B = planta_usable.data[i+2];
+        float ExG = 1.9 * G - R - B; //filtrado por exceso de verdes: 
+        if(ExG > 50){
+            planta_vectorizada.data[j]=255;
+        } else{
+            planta_vectorizada.data[j]=0;
+        }
+
+    }
+
+    planta_vectorizada.write("planta_generada2.png"); 
+    
+    system("pause");
+    return 0;
+}
--- Procesamiento_Imagenes/prueba_escala_de_grises.cpp
+++ Procesamiento_Imagenes/prueba_escala_de_grises.cpp
@@ -6,9 +6,9 @@ int main(){
     test.grayscale_promedio();
     test.write("escala_grises.png");
     printf("canales %d  \n", test.channels);
-    for (int i=0; i<test.size ; i++){
-        printf("%d", test.data[i]);
-    }
+    //for (int i=0; i<test.size ; i++){
+      //  printf("%d", test.data[i]);
+    //}
     system("pause");
    
     return 0;
